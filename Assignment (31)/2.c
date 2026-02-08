90d46ffd31a043a88b56468a0e15306e89d4c43e
Siddhant Gadkari <gadkari.siddhant.vivek@gmail.com>
2026-02-08 23:21:41 +0530

    Initial Upload

---
+++ Assignment (31)/2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0; int j = 0;
+
+    for(i=0; i<iRow; i++)
+    {
+        for(j=0; j<iCol; j++)
+        {
+            if(i <= j)
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0; int iValue2 = 0;
+
+    printf("Enter the Row:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Column:");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
