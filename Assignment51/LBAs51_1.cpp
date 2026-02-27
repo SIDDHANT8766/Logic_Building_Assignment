b1100cf8bd6399232c853c333d5f0be77bc23b55
Siddhant Gadkari <gadkari.siddhant.vivek@gmail.com>
2026-02-28 00:17:57 +0530

    Initial Upload

---
+++ Assignment51/LBAs51_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+template<class T> 
+
+void Display(T value , int Size)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= Size; iCnt++)
+    {
+        cout<< value << " ";
+    }
+    cout<<"\n";
+}
+
+int main()
+{
+    Display('M',7);
+    Display(11,3);
+    Display(3.7,6);
+
+    return 0;
+}
---
+++ Assignment51/LBAs51_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+int SearchFirst(T *arr, int iSize, T iNo)
+{
+    for(int i = 0; i < iSize; i++)
+    {
+        if(arr[i] == iNo)
+        {
+            return i + 1;   // Position
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+
+    int iRet = SearchFirst(arr, 9, 40);
+    cout << iRet;   // Output: 6
+
+    return 0;
+}
